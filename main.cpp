7ee35cf9df6ca64d537fd16431f2487b080866ac
kgr-1019 <k023g0009@g.neec.ac.jp>
2024-04-26 13:08:50 +0900

    MT3_01_02

--- main.cpp
+++ main.cpp
@@ -1,6 +1,40 @@
 #include <Novice.h>
+#include"Matrix4x4.h"
+#include"Vector3.h"
+#include<cmath>
 
 const char kWindowTitle[] = "LD2B_04_コマツザキ_カガリ_タイトル";
+
+
+// 球
+struct Sphere
+{
+	Vector3 center;// 中心点
+	float radius;// 半径
+};
+
+
+// プロトタイプ宣言
+void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix);
+void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color);
+
+
+// 関数の定義
+
+// Gridを表示する疑似コード
+void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix)
+{
+	const float kGridHalfWidth = 2.0f;// Gridの半分の幅
+	const uint32_t kSubdivision = 10;// 分割数
+	const float kGridEvery = (kGridHalfWidth * 2.0f) / float(kSubdivision);// 1つ分の長さ
+
+	// 奥から手前への線を順々に引いていく
+	for (uint32_t xIndex = 0; xIndex <= kSubdivision; ++xIndex)
+	{
+		// 上の情報を使ってワールド座標系上の始点と終点を求める
+
+	}
+}
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -11,6 +45,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
+
+
+	// カメラ
+	Vector3 cameraTranslate{ 0.0f,1.9f,-6.49f };// カメラの位置
+	Vector3 cameraRotate{ 0.26f,0.0f,0.0f };// カメラの角度
+
+
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
