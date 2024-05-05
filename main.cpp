100857c7687d3272d3540b97554f3695dd250c32
kgr-1019 <k023g0009@g.neec.ac.jp>
2024-05-06 01:47:16 +0900

    MT3_01_02

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include"Matrix4x4.h"
 #include"Vector3.h"
 #include<cmath>
+#include"assert.h"
+#define _USE_MATH_DEFINES
+#include <math.h>
 
 const char kWindowTitle[] = "LD2B_04_コマツザキ_カガリ_タイトル";
 
@@ -17,9 +20,29 @@ struct Sphere
 // プロトタイプ宣言
 void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix);
 void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color);
+// 座標変換
+Vector3 Transform(const Vector3& point, const Matrix4x4& transformMatrix);
 
 
 // 関数の定義
+
+// 座標変換
+Vector3 Transform(const Vector3& point, const Matrix4x4& transformMatrix)
+{
+	Vector3 result{};
+
+	result.x = point.x * transformMatrix.m[0][0] + point.y * transformMatrix.m[1][0] + point.z * transformMatrix.m[2][0] + 1.0f * transformMatrix.m[3][0];
+	result.y = point.x * transformMatrix.m[0][1] + point.y * transformMatrix.m[1][1] + point.z * transformMatrix.m[2][1] + 1.0f * transformMatrix.m[3][1];
+	result.z = point.x * transformMatrix.m[0][2] + point.y * transformMatrix.m[1][2] + point.z * transformMatrix.m[2][2] + 1.0f * transformMatrix.m[3][2];
+	float w = point.x * transformMatrix.m[0][3] + point.y * transformMatrix.m[1][3] + point.z * transformMatrix.m[2][3] + 1.0f * transformMatrix.m[3][3];
+	assert(w != 0.0f);
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
+
+	return result;
+}
+
 
 // Gridを表示する疑似コード
 void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix)
@@ -32,9 +55,33 @@ void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMa
 	for (uint32_t xIndex = 0; xIndex <= kSubdivision; ++xIndex)
 	{
 		// 上の情報を使ってワールド座標系上の始点と終点を求める
-
+		Vector3 start=Transform(Transform())
 	}
 }
+
+
+void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color)
+{
+	const uint32_t kSubdivision = 10;// 分割数
+	const float kLonEvery = (2.0f * float(M_PI)) / kSubdivision;// 経度分割1つ分の角度
+	const float kLatEvery = float(M_PI) / kSubdivision;// 緯度分割1つ分の角度
+	// 経度の方向に分割 -π/2 ～ π/2
+	for (uint32_t latIndex = 0; latIndex < kSubdivision; ++latIndex)
+	{
+		float lat = -float(M_PI) / 2.0f + kLatEvery * latIndex;// 現在の緯度
+		// 経度の方向に分割 0 ～ 2π
+		for (uint32_t lonIndex = 0; lonIndex < kSubdivision; ++lonIndex)
+		{
+			float lon = lonIndex * kLonEvery;// 現在の経度
+			// world座標系でのa,b,cを求める
+			Vector3 a, b, c;
+			// a,b,cをScreen座標系まで変換
+
+			// ab,bcで線を引く
+		}
+	}
+}
+
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
