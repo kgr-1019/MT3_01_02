d152dc7b6e5811f8c614bd905406a24713110731
kgr-1019 <k023g0009@g.neec.ac.jp>
2024-05-10 01:59:29 +0900

    MT3_01_02

--- main.cpp
+++ main.cpp
@@ -20,11 +20,259 @@ struct Sphere
 // プロトタイプ宣言
 void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix);
 void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color);
+// 平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+// X軸回転行列
+Matrix4x4 MakeRotateXMatrix(const Vector3& rotate);
+// Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(Vector3 rotate);
+// Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(Vector3 rotate);
+// XYZ合成
+Matrix4x4 Multiply(const Matrix4x4& rotateX, const Matrix4x4& rotateYZ);
+// アフィン変換
+Matrix4x4 MakeAffineMatrix(const Vector3& S, const Vector3& R, const Vector3& T);
+// 逆行列
+Matrix4x4 Inverse(Matrix4x4 cameraMatrix);
+// 透視投影行列
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
+// ビューポート行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
 // 座標変換
 Vector3 Transform(const Vector3& point, const Matrix4x4& transformMatrix);
 
 
+
 // 関数の定義
+
+// 平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate)
+{
+	Matrix4x4 result{};
+
+	for (int i = 0; i < 4; i++)
+	{
+		result.m[i][i] = 1;
+	}
+	result.m[3][0] = translate.x;
+	result.m[3][1] = translate.y;
+	result.m[3][2] = translate.z;
+
+	return result;
+}
+
+// X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float rotate)
+{
+	Matrix4x4 result{};
+
+	result.m[1][1] = std::cos(rotate);
+	result.m[1][2] = std::sin(rotate);
+	result.m[2][1] = -std::sin(rotate);
+	result.m[2][2] = std::cos(rotate);
+	result.m[0][0] = 1;
+	result.m[3][3] = 1;
+
+	return result;
+}
+
+// Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float rotate)
+{
+	Matrix4x4 result{};
+
+	result.m[0][0] = std::cos(rotate);
+	result.m[0][2] = -std::sin(rotate);
+	result.m[1][1] = 1;
+	result.m[2][0] = std::sin(rotate);
+	result.m[2][2] = std::cos(rotate);
+	result.m[3][3] = 1;
+
+	return result;
+}
+
+// Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float rotate)
+{
+	Matrix4x4 result{};
+
+	result.m[0][0] = std::cos(rotate);
+	result.m[0][1] = std::sin(rotate);
+	result.m[1][0] = -std::sin(rotate);
+	result.m[1][1] = std::cos(rotate);
+	result.m[2][2] = 1;
+	result.m[3][3] = 1;
+
+	return result;
+}
+
+// XYZ合成
+Matrix4x4 Multiply(const Matrix4x4& rotateX, const Matrix4x4& rotateYZ)
+{
+	Matrix4x4 result{};
+
+	for (int i = 0; i < 4; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			for (int k = 0; k < 4; k++)
+			{
+				result.m[i][j] += rotateX.m[i][k] * rotateYZ.m[k][j];
+			}
+		}
+	}
+
+	return result;
+}
+
+// アフィン変換
+Matrix4x4 MakeAffineMatrix(const Vector3& S, const Vector3& R, const Vector3& T)
+{
+	Matrix4x4 result{};
+
+	Matrix4x4 rotateXMatrix = MakeRotateXMatrix(R.x);
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(R.y);
+	Matrix4x4 rotateZMatrix = MakeRotateZMatrix(R.z);
+	Matrix4x4 rotateXYZMatrix = Multiply(rotateXMatrix, Multiply(rotateYMatrix, rotateZMatrix));
+
+
+	result.m[0][0] = S.x * rotateXYZMatrix.m[0][0];
+	result.m[0][1] = S.x * rotateXYZMatrix.m[0][1];
+	result.m[0][2] = S.x * rotateXYZMatrix.m[0][2];
+	result.m[1][0] = S.y * rotateXYZMatrix.m[1][0];
+	result.m[1][1] = S.y * rotateXYZMatrix.m[1][1];
+	result.m[1][2] = S.y * rotateXYZMatrix.m[1][2];
+	result.m[2][0] = S.z * rotateXYZMatrix.m[2][0];
+	result.m[2][1] = S.z * rotateXYZMatrix.m[2][1];
+	result.m[2][2] = S.z * rotateXYZMatrix.m[2][2];
+	result.m[3][0] = T.x;
+	result.m[3][1] = T.y;
+	result.m[3][2] = T.z;
+	result.m[3][3] = 1;
+
+	return result;
+}
+
+// 逆行列
+Matrix4x4 Inverse(Matrix4x4 cameraMatrix)
+{
+	Matrix4x4 result{};
+
+	float abs;//絶対値はint型にする
+
+	// |A|
+	abs = (cameraMatrix.m[0][0] * cameraMatrix.m[1][1] * cameraMatrix.m[2][2] * cameraMatrix.m[3][3]) + (cameraMatrix.m[0][0] * cameraMatrix.m[1][2] * cameraMatrix.m[2][3] * cameraMatrix.m[3][1]) + (cameraMatrix.m[0][0] * cameraMatrix.m[1][3] * cameraMatrix.m[2][1] * cameraMatrix.m[3][2])
+		- (cameraMatrix.m[0][0] * cameraMatrix.m[1][3] * cameraMatrix.m[2][2] * cameraMatrix.m[3][1]) - (cameraMatrix.m[0][0] * cameraMatrix.m[1][2] * cameraMatrix.m[2][1] * cameraMatrix.m[3][3]) - (cameraMatrix.m[0][0] * cameraMatrix.m[1][1] * cameraMatrix.m[2][3] * cameraMatrix.m[3][2])
+		- (cameraMatrix.m[0][1] * cameraMatrix.m[1][0] * cameraMatrix.m[2][2] * cameraMatrix.m[3][3]) - (cameraMatrix.m[0][2] * cameraMatrix.m[1][0] * cameraMatrix.m[2][3] * cameraMatrix.m[3][1]) - (cameraMatrix.m[0][3] * cameraMatrix.m[1][0] * cameraMatrix.m[2][1] * cameraMatrix.m[3][2])
+		+ (cameraMatrix.m[0][3] * cameraMatrix.m[1][0] * cameraMatrix.m[2][2] * cameraMatrix.m[3][1]) + (cameraMatrix.m[0][2] * cameraMatrix.m[1][0] * cameraMatrix.m[2][1] * cameraMatrix.m[3][3]) + (cameraMatrix.m[0][1] * cameraMatrix.m[1][0] * cameraMatrix.m[2][3] * cameraMatrix.m[3][2])
+		+ (cameraMatrix.m[0][1] * cameraMatrix.m[1][2] * cameraMatrix.m[2][0] * cameraMatrix.m[3][3]) + (cameraMatrix.m[0][2] * cameraMatrix.m[1][3] * cameraMatrix.m[2][0] * cameraMatrix.m[3][1]) + (cameraMatrix.m[0][3] * cameraMatrix.m[1][1] * cameraMatrix.m[2][0] * cameraMatrix.m[3][2])
+		- (cameraMatrix.m[0][3] * cameraMatrix.m[1][2] * cameraMatrix.m[2][0] * cameraMatrix.m[3][1]) - (cameraMatrix.m[0][2] * cameraMatrix.m[1][1] * cameraMatrix.m[2][0] * cameraMatrix.m[3][3]) - (cameraMatrix.m[0][1] * cameraMatrix.m[1][3] * cameraMatrix.m[2][0] * cameraMatrix.m[3][2])
+		- (cameraMatrix.m[0][1] * cameraMatrix.m[1][2] * cameraMatrix.m[2][3] * cameraMatrix.m[3][0]) - (cameraMatrix.m[0][2] * cameraMatrix.m[1][3] * cameraMatrix.m[2][1] * cameraMatrix.m[3][0]) - (cameraMatrix.m[0][3] * cameraMatrix.m[1][1] * cameraMatrix.m[2][2] * cameraMatrix.m[3][0])
+		+ (cameraMatrix.m[0][3] * cameraMatrix.m[1][2] * cameraMatrix.m[2][1] * cameraMatrix.m[3][0]) + (cameraMatrix.m[0][2] * cameraMatrix.m[1][1] * cameraMatrix.m[2][3] * cameraMatrix.m[3][0]) + (cameraMatrix.m[0][1] * cameraMatrix.m[1][3] * cameraMatrix.m[2][2] * cameraMatrix.m[3][0]
+			);
+
+	// 1/A
+	result.m[0][0] = 1.0f / abs * (
+		(cameraMatrix.m[1][1] * cameraMatrix.m[2][2] * cameraMatrix.m[3][3]) + (cameraMatrix.m[1][2] * cameraMatrix.m[2][3] * cameraMatrix.m[3][1]) + (cameraMatrix.m[1][3] * cameraMatrix.m[2][1] * cameraMatrix.m[3][2])
+		- (cameraMatrix.m[1][3] * cameraMatrix.m[2][2] * cameraMatrix.m[3][1]) - (cameraMatrix.m[1][2] * cameraMatrix.m[2][1] * cameraMatrix.m[3][3]) - (cameraMatrix.m[1][1] * cameraMatrix.m[2][3] * cameraMatrix.m[3][2])
+		);
+	result.m[0][1] = 1.0f / abs * (
+		-(cameraMatrix.m[0][1] * cameraMatrix.m[2][2] * cameraMatrix.m[3][3]) - (cameraMatrix.m[0][2] * cameraMatrix.m[2][3] * cameraMatrix.m[3][1]) - (cameraMatrix.m[0][3] * cameraMatrix.m[2][1] * cameraMatrix.m[3][2])
+		+ cameraMatrix.m[0][3] * cameraMatrix.m[2][2] * cameraMatrix.m[3][1] + cameraMatrix.m[0][2] * cameraMatrix.m[2][1] * cameraMatrix.m[3][3] + cameraMatrix.m[0][1] * cameraMatrix.m[2][3] * cameraMatrix.m[3][2]
+		);
+	result.m[0][2] = 1.0f / abs * (
+		(cameraMatrix.m[0][1] * cameraMatrix.m[1][2] * cameraMatrix.m[3][3]) + (cameraMatrix.m[0][2] * cameraMatrix.m[1][3] * cameraMatrix.m[3][1]) + (cameraMatrix.m[0][3] * cameraMatrix.m[1][1] * cameraMatrix.m[3][2])
+		- (cameraMatrix.m[0][3] * cameraMatrix.m[1][2] * cameraMatrix.m[3][1]) - (cameraMatrix.m[0][2] * cameraMatrix.m[1][1] * cameraMatrix.m[3][3]) - (cameraMatrix.m[0][1] * cameraMatrix.m[1][3] * cameraMatrix.m[3][2])
+		);
+	result.m[0][3] = 1.0f / abs * (
+		-(cameraMatrix.m[0][1] * cameraMatrix.m[1][2] * cameraMatrix.m[2][3]) - (cameraMatrix.m[0][2] * cameraMatrix.m[1][3] * cameraMatrix.m[2][1]) - (cameraMatrix.m[0][3] * cameraMatrix.m[1][1] * cameraMatrix.m[2][2])
+		+ (cameraMatrix.m[0][3] * cameraMatrix.m[1][2] * cameraMatrix.m[2][1]) + (cameraMatrix.m[0][2] * cameraMatrix.m[1][1] * cameraMatrix.m[2][3]) + (cameraMatrix.m[0][1] * cameraMatrix.m[1][3] * cameraMatrix.m[2][2])
+		);
+
+	result.m[1][0] = 1.0f / abs * (
+		-(cameraMatrix.m[1][0] * cameraMatrix.m[2][2] * cameraMatrix.m[3][3]) - (cameraMatrix.m[1][2] * cameraMatrix.m[2][3] * cameraMatrix.m[3][0]) - (cameraMatrix.m[1][3] * cameraMatrix.m[2][0] * cameraMatrix.m[3][2])
+		+ (cameraMatrix.m[1][3] * cameraMatrix.m[2][2] * cameraMatrix.m[3][0]) + (cameraMatrix.m[1][2] * cameraMatrix.m[2][0] * cameraMatrix.m[3][3]) + (cameraMatrix.m[1][0] * cameraMatrix.m[2][3] * cameraMatrix.m[3][2])
+		);
+	result.m[1][1] = 1.0f / abs * (
+		(cameraMatrix.m[0][0] * cameraMatrix.m[2][2] * cameraMatrix.m[3][3]) + (cameraMatrix.m[0][2] * cameraMatrix.m[2][3] * cameraMatrix.m[3][0]) + (cameraMatrix.m[0][3] * cameraMatrix.m[2][0] * cameraMatrix.m[3][2])
+		- (cameraMatrix.m[0][3] * cameraMatrix.m[2][2] * cameraMatrix.m[3][0]) - (cameraMatrix.m[0][2] * cameraMatrix.m[2][0] * cameraMatrix.m[3][3]) - (cameraMatrix.m[0][0] * cameraMatrix.m[2][3] * cameraMatrix.m[3][2])
+		);
+	result.m[1][2] = 1.0f / abs * (
+		-(cameraMatrix.m[0][0] * cameraMatrix.m[1][2] * cameraMatrix.m[3][3]) - (cameraMatrix.m[0][2] * cameraMatrix.m[1][3] * cameraMatrix.m[3][0]) - (cameraMatrix.m[0][3] * cameraMatrix.m[1][0] * cameraMatrix.m[3][2])
+		+ (cameraMatrix.m[0][3] * cameraMatrix.m[1][2] * cameraMatrix.m[3][0]) + (cameraMatrix.m[0][2] * cameraMatrix.m[1][0] * cameraMatrix.m[3][3]) + (cameraMatrix.m[0][0] * cameraMatrix.m[1][3] * cameraMatrix.m[3][2])
+		);
+	result.m[1][3] = 1.0f / abs * (
+		(cameraMatrix.m[0][0] * cameraMatrix.m[1][2] * cameraMatrix.m[2][3]) + (cameraMatrix.m[0][2] * cameraMatrix.m[1][3] * cameraMatrix.m[2][0]) + (cameraMatrix.m[0][3] * cameraMatrix.m[1][0] * cameraMatrix.m[2][2])
+		- (cameraMatrix.m[0][3] * cameraMatrix.m[1][2] * cameraMatrix.m[2][0]) - (cameraMatrix.m[0][2] * cameraMatrix.m[1][0] * cameraMatrix.m[2][3]) - (cameraMatrix.m[0][0] * cameraMatrix.m[1][3] * cameraMatrix.m[2][2])
+		);
+
+	result.m[2][0] = 1.0f / abs * (
+		(cameraMatrix.m[1][0] * cameraMatrix.m[2][1] * cameraMatrix.m[3][3]) + (cameraMatrix.m[1][1] * cameraMatrix.m[2][3] * cameraMatrix.m[3][0]) + (cameraMatrix.m[1][3] * cameraMatrix.m[2][0] * cameraMatrix.m[3][1])
+		- (cameraMatrix.m[1][3] * cameraMatrix.m[2][1] * cameraMatrix.m[3][0]) - (cameraMatrix.m[1][1] * cameraMatrix.m[2][0] * cameraMatrix.m[3][3]) - (cameraMatrix.m[1][0] * cameraMatrix.m[2][3] * cameraMatrix.m[3][1])
+		);
+	result.m[2][1] = 1.0f / abs * (
+		-(cameraMatrix.m[0][0] * cameraMatrix.m[2][1] * cameraMatrix.m[3][3]) - (cameraMatrix.m[0][1] * cameraMatrix.m[2][3] * cameraMatrix.m[3][0]) - (cameraMatrix.m[0][3] * cameraMatrix.m[2][0] * cameraMatrix.m[3][1])
+		+ (cameraMatrix.m[0][3] * cameraMatrix.m[2][1] * cameraMatrix.m[3][0]) + (cameraMatrix.m[0][1] * cameraMatrix.m[2][0] * cameraMatrix.m[3][3]) + (cameraMatrix.m[0][0] * cameraMatrix.m[2][3] * cameraMatrix.m[3][1])
+		);
+	result.m[2][2] = 1.0f / abs * (
+		(cameraMatrix.m[0][0] * cameraMatrix.m[1][1] * cameraMatrix.m[3][3]) + (cameraMatrix.m[0][1] * cameraMatrix.m[1][3] * cameraMatrix.m[3][0]) + (cameraMatrix.m[0][3] * cameraMatrix.m[1][0] * cameraMatrix.m[3][1])
+		- (cameraMatrix.m[0][3] * cameraMatrix.m[1][1] * cameraMatrix.m[3][0]) - (cameraMatrix.m[0][1] * cameraMatrix.m[1][0] * cameraMatrix.m[3][3]) - (cameraMatrix.m[0][0] * cameraMatrix.m[1][3] * cameraMatrix.m[3][1])
+		);
+	result.m[2][3] = 1.0f / abs * (
+		-(cameraMatrix.m[0][0] * cameraMatrix.m[1][1] * cameraMatrix.m[2][3]) - (cameraMatrix.m[0][1] * cameraMatrix.m[1][3] * cameraMatrix.m[2][0]) - (cameraMatrix.m[0][3] * cameraMatrix.m[1][0] * cameraMatrix.m[2][1])
+		+ (cameraMatrix.m[0][3] * cameraMatrix.m[1][1] * cameraMatrix.m[2][0]) + (cameraMatrix.m[0][1] * cameraMatrix.m[1][0] * cameraMatrix.m[2][3]) + (cameraMatrix.m[0][0] * cameraMatrix.m[1][3] * cameraMatrix.m[2][1])
+		);
+
+	result.m[3][0] = 1.0f / abs * (
+		-(cameraMatrix.m[1][0] * cameraMatrix.m[2][1] * cameraMatrix.m[3][2]) - (cameraMatrix.m[1][1] * cameraMatrix.m[2][2] * cameraMatrix.m[3][0]) - (cameraMatrix.m[1][2] * cameraMatrix.m[2][0] * cameraMatrix.m[3][1])
+		+ (cameraMatrix.m[1][2] * cameraMatrix.m[2][1] * cameraMatrix.m[3][0]) + (cameraMatrix.m[1][1] * cameraMatrix.m[2][0] * cameraMatrix.m[3][2]) + (cameraMatrix.m[1][0] * cameraMatrix.m[2][2] * cameraMatrix.m[3][1])
+		);
+	result.m[3][1] = 1.0f / abs * (
+		(cameraMatrix.m[0][0] * cameraMatrix.m[2][1] * cameraMatrix.m[3][2]) + (cameraMatrix.m[0][1] * cameraMatrix.m[2][2] * cameraMatrix.m[3][0]) + (cameraMatrix.m[0][2] * cameraMatrix.m[2][0] * cameraMatrix.m[3][1])
+		- (cameraMatrix.m[0][2] * cameraMatrix.m[2][1] * cameraMatrix.m[3][0]) - (cameraMatrix.m[0][1] * cameraMatrix.m[2][0] * cameraMatrix.m[3][2]) - (cameraMatrix.m[0][0] * cameraMatrix.m[2][2] * cameraMatrix.m[3][1])
+		);
+	result.m[3][2] = 1.0f / abs * (
+		-(cameraMatrix.m[0][0] * cameraMatrix.m[1][1] * cameraMatrix.m[3][2]) - (cameraMatrix.m[0][1] * cameraMatrix.m[1][2] * cameraMatrix.m[3][0]) - (cameraMatrix.m[0][2] * cameraMatrix.m[1][0] * cameraMatrix.m[3][1])
+		+ (cameraMatrix.m[0][2] * cameraMatrix.m[1][1] * cameraMatrix.m[3][0]) + (cameraMatrix.m[0][1] * cameraMatrix.m[1][0] * cameraMatrix.m[3][2]) + (cameraMatrix.m[0][0] * cameraMatrix.m[1][2] * cameraMatrix.m[3][1])
+		);
+	result.m[3][3] = 1.0f / abs * (
+		(cameraMatrix.m[0][0] * cameraMatrix.m[1][1] * cameraMatrix.m[2][2]) + (cameraMatrix.m[0][1] * cameraMatrix.m[1][2] * cameraMatrix.m[2][0]) + (cameraMatrix.m[0][2] * cameraMatrix.m[1][0] * cameraMatrix.m[2][1])
+		- (cameraMatrix.m[0][2] * cameraMatrix.m[1][1] * cameraMatrix.m[2][0]) - (cameraMatrix.m[0][1] * cameraMatrix.m[1][0] * cameraMatrix.m[2][2]) - (cameraMatrix.m[0][0] * cameraMatrix.m[1][2] * cameraMatrix.m[2][1])
+		);
+
+
+	return result;
+}
+
+// 透視投影行列
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip)
+{
+	Matrix4x4 result{};
+
+	result.m[0][0] = (1 / aspectRatio) * 1 / std::tan(fovY / 2);
+	result.m[1][1] = 1 / std::tan(fovY / 2);
+	result.m[2][2] = farClip / (farClip - nearClip);
+	result.m[2][3] = 1;
+	result.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
+
+	return result;
+}
+
+// ビューポート変換行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth)
+{
+	Matrix4x4 result{};
+
+	result.m[0][0] = width / 2;
+	result.m[1][1] = -height / 2;
+	result.m[2][2] = maxDepth - minDepth;
+	result.m[3][0] = left + (width / 2);
+	result.m[3][1] = top + (height / 2);
+	result.m[3][2] = minDepth;
+	result.m[3][3] = 1;
+
+	return result;
+}
 
 // 座標変換
 Vector3 Transform(const Vector3& point, const Matrix4x4& transformMatrix)
@@ -51,36 +299,87 @@ void DrawGrid(const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMa
 	const uint32_t kSubdivision = 10;// 分割数
 	const float kGridEvery = (kGridHalfWidth * 2.0f) / float(kSubdivision);// 1つ分の長さ
 
+	Vector3 startLineZ;
+	Vector3 endLineZ;
+	Vector3 startLineX;
+	Vector3 endLineX;
+
 	// 奥から手前への線を順々に引いていく
 	for (uint32_t xIndex = 0; xIndex <= kSubdivision; ++xIndex)
 	{
 		// 上の情報を使ってワールド座標系上の始点と終点を求める
-		Vector3 start=Transform(Transform())
+		startLineZ = { xIndex * (kGridEvery - kGridHalfWidth), 0.0f, -60.0f };
+		endLineZ = { xIndex * (kGridEvery -kGridHalfWidth), 0.0f , 60.0f};
+
+		// スクリーン座標系まで変換をかける
+		Matrix4x4 worldMatrixStart = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, startLineZ);
+		Matrix4x4 worldMatrixEnd = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, endLineZ);
+
+		Matrix4x4 worldViewProjectionMatrixStart = Multiply(worldMatrixStart, viewProjectionMatrix);
+		Matrix4x4 worldViewProjectionMatrixEnd = Multiply(worldMatrixEnd, viewProjectionMatrix);
+
+		Vector3 ndcVertexStart = Transform(startLineZ, viewProjectionMatrix);
+		Vector3 ndcVertexEnd = Transform(endLineZ, viewProjectionMatrix);
+
+		Vector3 screenVerticesStart = Transform(ndcVertexStart, viewportMatrix);
+		Vector3 screenVerticesEnd = Transform(ndcVertexEnd, viewportMatrix);
+
+		// 変換した座標を使って表示
+		Novice::DrawLine((int)screenVerticesStart.x, (int)screenVerticesStart.y, (int)screenVerticesEnd.x, (int)screenVerticesEnd.y, 0xAAAAAAFF);
+
+		Novice::ScreenPrintf(20, 20, "screenStart.x=%f", screenVerticesStart.x);
+		Novice::ScreenPrintf(20, 35, "screenStart.y=%f", screenVerticesStart.y);
+		Novice::ScreenPrintf(20, 50, "screenStart.z=%f", screenVerticesStart.z);
+		Novice::ScreenPrintf(20, 65, "screenEnd.x=%f", screenVerticesEnd.x);
+		Novice::ScreenPrintf(20, 80, "screenEnd.y=%f", screenVerticesEnd.y);
+		Novice::ScreenPrintf(20, 95, "screenEnd.z=%f", screenVerticesEnd.z);
 	}
-}
 
-
-void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color)
-{
-	const uint32_t kSubdivision = 10;// 分割数
-	const float kLonEvery = (2.0f * float(M_PI)) / kSubdivision;// 経度分割1つ分の角度
-	const float kLatEvery = float(M_PI) / kSubdivision;// 緯度分割1つ分の角度
-	// 経度の方向に分割 -π/2 ～ π/2
-	for (uint32_t latIndex = 0; latIndex < kSubdivision; ++latIndex)
+	// 左から右
+	for (uint32_t zIndex = 0; zIndex <= kSubdivision; ++zIndex)
 	{
-		float lat = -float(M_PI) / 2.0f + kLatEvery * latIndex;// 現在の緯度
-		// 経度の方向に分割 0 ～ 2π
-		for (uint32_t lonIndex = 0; lonIndex < kSubdivision; ++lonIndex)
-		{
-			float lon = lonIndex * kLonEvery;// 現在の経度
-			// world座標系でのa,b,cを求める
-			Vector3 a, b, c;
-			// a,b,cをScreen座標系まで変換
 
-			// ab,bcで線を引く
-		}
+		// 上の情報を使ってワールド座標系上の始点と終点を求める
+		startLineX = { 60.0f, 0.0f, zIndex * (kGridEvery - kGridHalfWidth) };
+		endLineX = { 60.0f, 0.0f ,zIndex * (kGridEvery - kGridHalfWidth) };
+
+		// スクリーン座標系まで変換をかける
+		Matrix4x4 worldMatrixLeft = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, startLineX);
+		Matrix4x4 worldMatrixRight = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, { 0.0f,0.0f,0.0f }, endLineX);
+
+		Matrix4x4 worldViewProjectionMatrixLeft = Multiply(worldMatrixLeft, viewProjectionMatrix);
+		Matrix4x4 worldViewProjectionMatrixRight = Multiply(worldMatrixRight, viewProjectionMatrix);
+
+		Vector3 ndcVertexLeft = Transform(startLineX, viewProjectionMatrix);
+		Vector3 ndcVertexRight = Transform(endLineX, viewProjectionMatrix);
+
+		Vector3 screenVerticesLeft = Transform(ndcVertexLeft, viewportMatrix);
+		Vector3 screenVerticesRight = Transform(ndcVertexRight, viewportMatrix);
 	}
 }
+
+
+//void DrawSphere(const Sphere& sphere, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color)
+//{
+//	const uint32_t kSubdivision = 10;// 分割数
+//	const float kLonEvery = (2.0f * float(M_PI)) / kSubdivision;// 経度分割1つ分の角度
+//	const float kLatEvery = float(M_PI) / kSubdivision;// 緯度分割1つ分の角度
+//	// 経度の方向に分割 -π/2 ～ π/2
+//	for (uint32_t latIndex = 0; latIndex < kSubdivision; ++latIndex)
+//	{
+//		float lat = -float(M_PI) / 2.0f + kLatEvery * latIndex;// 現在の緯度
+//		// 経度の方向に分割 0 ～ 2π
+//		for (uint32_t lonIndex = 0; lonIndex < kSubdivision; ++lonIndex)
+//		{
+//			float lon = lonIndex * kLonEvery;// 現在の経度
+//			// world座標系でのa,b,cを求める
+//			Vector3 a, b, c;
+//			// a,b,cをScreen座標系まで変換
+//
+//			// ab,bcで線を引く
+//		}
+//	}
+//}
 
 
 // Windowsアプリでのエントリーポイント(main関数)
@@ -94,10 +393,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	char preKeys[256] = {0};
 
 
+	Vector3 rotate{};
+	Vector3 translate{};
+
 	// カメラ
 	Vector3 cameraTranslate{ 0.0f,1.9f,-6.49f };// カメラの位置
 	Vector3 cameraRotate{ 0.26f,0.0f,0.0f };// カメラの角度
-
+	Vector3 cameraPosition{ 0.0f,0.0f,-300.0f };
 
 
 	// ウィンドウの×ボタンが押されるまでループ
@@ -113,6 +415,14 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		/// ↓更新処理ここから
 		///
 
+		Matrix4x4 worldMatrix = MakeAffineMatrix({ 1.0f,1.0f,1.0f }, rotate, translate);
+		Matrix4x4 viewMatrix = Inverse(worldMatrix);
+		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, 1280.0f / 720.0f, 0.1f, 100.0f);
+		Matrix4x4 viewProjectionMatrix = Multiply(viewMatrix, projectionMatrix);
+		Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, viewProjectionMatrix);
+		Matrix4x4 viewportMatrix = MakeViewportMatrix(0, 0, 1280.0f, 720, 0.0f, 1.0f);
+		DrawGrid(worldViewProjectionMatrix, viewportMatrix);
+
 		///
 		/// ↑更新処理ここまで
 		///
@@ -120,6 +430,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
+
+		
 
 		///
 		/// ↑描画処理ここまで
